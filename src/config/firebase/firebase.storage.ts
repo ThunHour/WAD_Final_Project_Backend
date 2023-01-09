47849af07f2bc02ff347b5047538832df5f07910
SeakKimhour <seakkimhor20@kit.edu.kh>
2023-01-09 07:10:55 +0700

    complete on upload image

---
+++ src/config/firebase/firebase.storage.ts
@@ -0,0 +1,10 @@
+import firebaseAdmin from "firebase-admin";
+const ServiceAccount = require("./storage.json");
+
+const admin = firebaseAdmin.initializeApp({
+  credential: firebaseAdmin.credential.cert(ServiceAccount),
+});
+
+module.exports = () => {
+  return admin.storage().bucket("wad-final-image-storage.appspot.com");
+};
