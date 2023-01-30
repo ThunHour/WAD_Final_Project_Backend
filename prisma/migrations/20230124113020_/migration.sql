eb544775781cc94b8039fb6faa279b1857c3a189
SrunChina <china.srunn@gmail.com>
2023-01-30 16:04:33 +0700

    storage

--- prisma/migrations/20230124113020_/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Ram" DROP CONSTRAINT "Ram_panelRamId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Ram" ADD CONSTRAINT "Ram_panelRamId_fkey" FOREIGN KEY ("panelRamId") REFERENCES "PanelRam"("id") ON DELETE CASCADE ON UPDATE CASCADE;
