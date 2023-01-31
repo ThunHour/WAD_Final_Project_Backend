cecf0e83fc810e0f351f2c21e04de2889d924631
SrunChina <china.srunn@gmail.com>
2023-01-31 22:07:39 +0700

    index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -6,6 +6,10 @@ import category from "./category/index";
 import cases from "./case/index";
 import ram from "./ram/index";
 import motherBoard from "./motherBoard/index";
+import storage from "./storage/index";
+import gpu from "./gpu/index";
+import powerSupply from "./powerSupply/index";
+
 export default (app: Application) => {
   const route = Router();
   route.use("/auth", auth());
@@ -15,6 +19,9 @@ export default (app: Application) => {
   route.use("/case", cases());
   route.use("/ram", ram());
   route.use("/motherBoard", motherBoard());
+  route.use("/storage", storage());
+  route.use("/gpu", gpu());
+  route.use("/powerSupply", powerSupply());
   app.use("/", route);
   return app;
 };
