2a14eaad54bcc0368fd2d3d67c7b1de337b5b78a
SeakKimhour <seakkimhor20@kit.edu.kh>
2023-02-02 20:59:33 +0700

    add register

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import loaders from "./loaders";
+import config from "./config/config";
 function app() {
   const app = express();
   loaders(app);
-  const port = 3000;
+  const port = config.PORT || 3000;
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
