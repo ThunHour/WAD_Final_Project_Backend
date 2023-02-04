0ae24163ab710f3eeed826bb34f156203c7ebb66
SeakKimhour <seakkimhor20@kit.edu.kh>
2023-02-04 23:26:53 +0700

    add register

--- src/loaders/expressLoader.ts
+++ src/loaders/expressLoader.ts
@@ -4,11 +4,9 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 import { rateLimiterUsingThirdParty } from "../middleware/ratelimite";
 const eurekaHelper = require("../config/eureka/eureka-helper");
-import config from "../config/config";
 export default (app: Application) => {
-  // Application routing
   // eurekaHelper.registerWithEureka("main-service", config.PORT || 3000);
-
+  require("dotenv").config();
   app.use(rateLimiterUsingThirdParty);
   app.use(cors());
   app.use(json());
